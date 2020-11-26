9bc309706b8fab33179cfd3249bdfe1171a0c930
Palenske <viniciuspalenske@gmail.com>
2020-11-26 03:01:42 -0300

    atual exe #9 #10 #11

--- bloco_4/dia_1/exercise_1.js
+++ bloco_4/dia_1/exercise_1.js
@@ -8,6 +8,7 @@ console.log('Divisão:', a/b);
 console.log('Módulo:', a%b);
 console.log('--------------------');
 console.log('\n');
+
 console.log('-----PROGRAMA_2-----');
 if (a == b){
     console.log('Números iguais');
@@ -18,6 +19,7 @@ if (a == b){
 }
 console.log('--------------------');
 console.log('\n');
+
 console.log('-----PROGRAMA_3-----');
 a = 0, b = 6; 
 let c = 10;
@@ -32,6 +34,7 @@ if (a == b || a == c || b == c){
 }
 console.log('--------------------');
 console.log('\n');
+
 console.log('-----PROGRAMA_4-----');
 if (a > 0){
     console.log('positive\n',a);
@@ -42,6 +45,7 @@ if (a > 0){
 }
 console.log('--------------------');
 console.log('\n');
+
 console.log('-----PROGRAMA_5-----');
 a = 80, b = 50, c = 50;
 console.log('Ângulo A:', a);
@@ -58,6 +62,7 @@ console.log(Boolean);
 
 console.log('--------------------');
 console.log('\n');
+
 console.log('-----PROGRAMA_6-----');
 let piece = 'KinG';
 piece = piece.toLocaleLowerCase();
@@ -85,6 +90,7 @@ switch (piece){
 }
 console.log('--------------------');
 console.log('\n');
+
 console.log('-----PROGRAMA_7-----');
 a = 89;
 if (a < 0 || a > 100){
@@ -105,4 +111,61 @@ else if (a >= 90){
 } 
 console.log('--------------------');
 console.log('\n');
+
 console.log('-----PROGRAMA_8-----');
+a = 3, b = 3, c = 2;
+Boolean = false;
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0){
+    Boolean = true;
+}
+console.log(Boolean);
+console.log('--------------------');
+console.log('\n');
+
+console.log('-----PROGRAMA_9-----');
+a = 2, b = 2, c = 3;
+Boolean = false;
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0){
+    Boolean = true;
+}
+console.log(Boolean);
+console.log('---------------------');
+console.log('\n');
+
+console.log('-----PROGRAMA_10-----');
+let valorCusto = 300.00, valorVenda = 500.00, imposto = 0.02; 
+let valorCustoTotal = valorCusto - (valorCusto*imposto); 
+let lucro = valorVenda - valorCustoTotal;
+let vendas = 1000;
+console.log('R$',vendas*lucro);
+console.log('---------------------');
+console.log('\n');
+
+console.log('-----PROGRAMA_11-----');
+let salarioBruto = 5000.00, salarioLiq = 0.0;
+
+if (salarioBruto > 5189.82){
+    inss = salarioBruto - 570.88;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82){
+    inss = salarioBruto - (salarioBruto * 0.11);
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92){
+    inss = salarioBruto - (salarioBruto * 0.09);
+} else if (salarioBruto > 0 && salarioBruto <= 1556.94){
+    inss = salarioBruto - (salarioBruto * 0.08);
+} else {
+    console.log('valor inválido');
+}
+ if (inss > 4664.68){
+     ir = (inss * 0.275) - 869.36;
+ } else if (inss >= 3751.06 && inss <= 4664.68){
+     ir = (inss * 0.225) - 636.13;
+ } else if (inss >= 2826.66 && inss <= 3751.05){
+     ir = (inss * 0.15) - 354.80;
+ } else if (inss >= 1903.99 && inss <= 2826.65){
+     ir = (inss * 0.075) - 142.80;
+ }
+ salarioLiq = inss - ir;
+ console.log('Bruto', salarioBruto);
+ console.log('INSS:', inss);
+ console.log('IR:', ir);
+ console.log('Liquido:', salarioLiq);
