c33abad48eb2ff05fe2176a4fe9d80e7a8312169
Vinicius Palenske <viniciuspalenske@gmail.com>
2020-12-26 20:29:03 -0300

    entrega exercicio 3

--- 5_JavaScript:_DOM_Events_and_Web_Storage/3_events/script.js
+++ 5_JavaScript:_DOM_Events_and_Web_Storage/3_events/script.js
@@ -46,5 +46,18 @@ holidayButton('Feriados');
 
 // exercise 3:
 function showHolidays() {
-  // test
+  function changeColor() {
+    const allHolidays = document.getElementsByClassName('holiday');
+    for (let index = 0; index < allHolidays.length; index += 1) {
+      if (allHolidays[index].style.backgroundColor != 'pink') {
+        allHolidays[index].style.backgroundColor = 'pink';
+      } else {
+        allHolidays[index].style.backgroundColor = 'rgb(238,238,238)';
+      }
+    }
+  }
+  const btnChange = document.querySelector('#btn-holiday');
+  btnChange.addEventListener('click', changeColor);
 }
+
+showHolidays();
