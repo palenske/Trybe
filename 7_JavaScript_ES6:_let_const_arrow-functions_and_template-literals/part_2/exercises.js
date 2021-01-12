8a5482b731eb6c41f25b0fb95462cc97963e861f
palenske <viniciuspalenske@gmail.com>
2021-01-12 19:26:20 -0300

    entrega exercicio 2 da parte 2

--- 7_JavaScript_ES6:_let_const_arrow-functions_and_template-literals/part_2/exercises.js
+++ 7_JavaScript_ES6:_let_const_arrow-functions_and_template-literals/part_2/exercises.js
@@ -1,3 +1,7 @@
-// Exercíse_1
+// Exercise_1
 const factorial = num => num === 0 || num === 1 ? num : num * factorial (num -1);
 console.log(factorial(4));
+
+// Exercise_2
+const highWord = phrase =>  phrase.split(' ').sort((a, b) => b.length - a.length)[0];
+console.log(highWord('Antonio foi no banheiro e não sabemos o que aconteceu'));
