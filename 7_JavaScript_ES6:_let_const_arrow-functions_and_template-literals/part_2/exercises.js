65a223fdca525bc7ac48466f093a6f1912f3792b
palenske <viniciuspalenske@gmail.com>
2021-01-13 18:02:06 -0300

    update branch 7.1

--- 7_JavaScript_ES6:_let_const_arrow-functions_and_template-literals/part_2/exercises.js
+++ 7_JavaScript_ES6:_let_const_arrow-functions_and_template-literals/part_2/exercises.js
@@ -1,7 +1,20 @@
 // Exercise_1
-const factorial = num => num === 0 || num === 1 ? num : num * factorial (num -1);
+const factorial = num => num === 0 || num === 1 ? 1 : num * factorial(num - 1);
 console.log(factorial(4));
 
 // Exercise_2
-const highWord = phrase =>  phrase.split(' ').sort((a, b) => b.length - a.length)[0];
+const highWord = phrase => phrase.split(' ').sort((a, b) => b.length - a.length)[0];
 console.log(highWord('Antonio foi no banheiro e não sabemos o que aconteceu'));
+
+
+// // Exercise_3
+// const listenerBtn = document.getElementById('button');
+// const clicks = document.getElementById('clicks');
+// let clickCount = 0;
+// listenerBtn.addEventListener('click', () => clicks.innerText = clickCount += 1);
+
+// Exercise_4
+const mySkills = ['JavaScript', 'HTML', 'CSS'];
+const changeX = word => `Tryber ${word} aqui!`;
+
+console.log(changeX('Vini'));
