a10f8141edd6a00d88074b46c7b5e336958f8be9
palenske <viniciuspalenske@gmail.com>
2021-01-18 10:08:35 -0300

    entrega exercicio 2, pt 2

--- 7_Introduction_to_JavaScript_ES6_and_Unit_Tests/2_objects_methods/exercise_2.js
+++ 7_Introduction_to_JavaScript_ES6_and_Unit_Tests/2_objects_methods/exercise_2.js
@@ -18,9 +18,15 @@ const lesson3 = {
   turno: 'noite',
 };
 
+// exercício 1
 const addkey = (obj, key, value) => {
   obj[key] = value;
   console.log(lesson2);
 }
 
 addkey(lesson2, 'turno', 'manhã');
+
+// exercício 2
+const listKeys = obj => Object.keys(obj);
+  console.log(listKeys(lesson1));
+
