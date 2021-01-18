58ed3cb6fe3ea1a35dbc44b47106b02c58cd5327
palenske <viniciuspalenske@gmail.com>
2021-01-18 09:58:42 -0300

    entrega exercício 1, pt 2

---
+++ 7_Introduction_to_JavaScript_ES6_and_Unit_Tests/2_objects_methods/exercise_2.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addkey = (obj, key, value) => {
+  obj[key] = value;
+  console.log(lesson2);
+}
+
+addkey(lesson2, 'turno', 'manhã');
