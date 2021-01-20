e119dae8bd86a084908598bb1f6105854188d997
palenske <viniciuspalenske@gmail.com>
2021-01-20 09:19:27 -0300

    entrega exercicio 5

--- 8_HighOrder_Function_JavaScript/2_HOF_map_filter/exercise_5.js
+++ 8_HighOrder_Function_JavaScript/2_HOF_map_filter/exercise_5.js
@@ -64,34 +64,17 @@ const books = [
 ];
 
 const expectedResult = [
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-    releaseYear: 1928
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
-  }
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien'
 ]
 
-function oldBooks() {
+function fantasyOrScienceFictionAuthors() {
   // escreva seu código aqui
-  const yearNow = new Date().getFullYear();
-  return books.filter((book) => (yearNow - book.releaseYear) >= 60).sort((a, b) => 
-  a.releaseYear - b.releaseYear);
+  return books.filter((book) => 
+  book.genre === 'Ficção Científica' || book.genre === 'Fantasia').map((book) =>
+  book.author.name).sort();
 }
 
-assert.deepStrictEqual(oldBooks(), expectedResult);
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
