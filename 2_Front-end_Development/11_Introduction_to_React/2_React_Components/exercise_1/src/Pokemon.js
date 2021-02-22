dca08ed0d0eebcc5c36ad1904191b93ca4653796
palenske <viniciuspalenske@gmail.com>
2021-02-22 18:48:53 -0300

    update branch

--- 2_Front-end_Development/11_Introduction_to_React/2_React_Components/exercise_1/src/Pokemon.js
+++ 2_Front-end_Development/11_Introduction_to_React/2_React_Components/exercise_1/src/Pokemon.js
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 class Pokemon extends React.Component {
   render() {
     const { id, name, type, averageWeight, image } = this.props.pokemon;
+    const { value, measurementUnit } = averageWeight;
 
     return (
-      <section className="pokemon">
+      <section tabIndex='0' className="pokemon">
         <img src={image} alt={`${name} sprite`} />
         <div>
           <p className='pokeName'> {`${name} #${id}`}</p>
           <p className={type}>{type}</p>
-          <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+          <p> {`Average weight: ${value} ${measurementUnit}`}</p>
         </div>
       </section>
     );
