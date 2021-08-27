d42abaa72ec2a4f485c7a470e5d14726603481ba
palenske <viniciuspalenske@gmail.com>
2021-08-27 14:37:07 -0300

    entrega exercicio 5

--- 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercise_05.js
+++ 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercise_05.js
@@ -2,7 +2,13 @@ const fs = require('fs').promises;
 
 async function multiWrite() {
   const array = ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'];
+  const fileNames = [ 'file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt' ];
+
   await Promise.all(array.map((word, index) => fs.writeFile(`./file${index + 1}.txt`, word)));
+  const files = await Promise.all(fileNames.map((file) => fs.readFile(file, 'utf-8')));
+  const newFile = files.join(' ');
+
+  await fs.writeFile('./fileAll.txt', newFile);
 }
 
 multiWrite();
