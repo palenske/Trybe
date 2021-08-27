8145391bf6a32fae31104c8e82197617f1e8f655
palenske <viniciuspalenske@gmail.com>
2021-08-26 23:38:02 -0300

    exercicio 4.2

--- 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercise_04.js
+++ 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercise_04.js
@@ -1,5 +1,17 @@
 const fs = require('fs').promises;
 
-fs.readFile('./simpsons.json', 'utf-8')
+async function showCharacters() {
+  return await fs.readFile('./simpsons.json', 'utf-8')
   .then((content) => JSON.parse(content))
   .then((characters) => characters.map(({ id, name }) => console.log(`${id} - ${name}`)));
+}
+
+async function charById(id) {
+  return await fs.readFile('./simpsons.json', 'utf-8')
+  .then((content) => JSON.parse(content))
+  .then((characters) => characters.find((char) => char.id == id))
+  .then(({ id, name }) => console.log(`${id} - ${name}`))
+  .catch(() => console.log('id não encontrado'));
+}
+
+charById(10);
