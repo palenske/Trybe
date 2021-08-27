30db6ab8cfba8baeb70c982cadac283c67ed85b7
palenske <viniciuspalenske@gmail.com>
2021-08-27 00:03:58 -0300

    exercicio 4.3

--- 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercise_04.js
+++ 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercise_04.js
@@ -1,12 +1,12 @@
 const fs = require('fs').promises;
 
-async function showCharacters() {
+async function showCharacters() { // 4.1
   return await fs.readFile('./simpsons.json', 'utf-8')
   .then((content) => JSON.parse(content))
   .then((characters) => characters.forEach(({ id, name }) => console.log(`${id} - ${name}`)));
 }
 
-async function charById(id) {
+async function charById(id) { // 4.2
   return await fs.readFile('./simpsons.json', 'utf-8')
   .then((content) => JSON.parse(content))
   .then((characters) => characters.find((char) => char.id == id))
@@ -14,12 +14,12 @@ async function charById(id) {
   .catch(() => console.log('id não encontrado'));
 }
 
-async function removeCharacter() {
+async function removeCharacter() { // 4.3
   return await fs.readFile('./simpsons.json', 'utf-8')
   .then((content) => JSON.parse(content))
-  .then((characters) => characters.filter(({id}) => id !== '6' && id !== '10'))
-  .then((characters) => characters.forEach(({ id, name }) => console.log(`${id} - ${name}`)));
+  .then((characters) => characters.forEach(({ id, name }) => {
+    if(id !== '6' && id !== '10') return console.log(`${id} - ${name}`)
+  }));
 }
-
 
 removeCharacter();
