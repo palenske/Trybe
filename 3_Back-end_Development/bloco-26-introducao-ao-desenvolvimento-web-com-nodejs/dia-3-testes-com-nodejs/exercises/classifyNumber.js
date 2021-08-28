64b4c8100c65fb0dff60495da7fc583bd7b2f91d
palenske <viniciuspalenske@gmail.com>
2021-08-28 15:19:44 -0300

    entrega exercicio 2

--- 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/exercises/classifyNumber.js
+++ 3_Back-end_Development/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/exercises/classifyNumber.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-
+module.exports = (num) => {
+  switch (true) {
+    case num > 0:
+      return "positivo"
+    case num < 0:
+      return "negativo"
+    default:
+      return "neutro"
+  }
 };
