70f7c9f9fef0a35e153fe79f02b56ea576447f92
palenske <viniciuspalenske@gmail.com>
2021-08-29 18:21:01 -0300

    entrega exercicio 10

---
+++ 3_Back-end_Development/bloco-25-mongodb-aggregation-framework/dia-1-aggregation-framework-parte-1/exercise_10.js
@@ -0,0 +1,18 @@
+db.vendas.aggregate([
+  {
+    $group: {
+      _id: "$clienteId",
+      totalCompras: {
+        $sum: 1
+      }
+    }
+  },
+  {
+    $match: {
+      totalCompras: { $gt: 5 }
+    }
+  },
+  {
+    $count: "totalClientes"
+  }
+]);
