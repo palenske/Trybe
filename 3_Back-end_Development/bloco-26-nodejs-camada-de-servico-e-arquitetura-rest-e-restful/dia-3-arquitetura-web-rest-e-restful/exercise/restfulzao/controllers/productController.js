ec4c81c376f34d09ddd1fa23c7980be340b0f550
palenske <viniciuspalenske@gmail.com>
2021-10-13 22:01:31 -0300

    padroniza endpoints de /products | padroniza retornos para JSON, array para coleçoes de recursos, objeto para um unico recurso

--- 3_Back-end_Development/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercise/restfulzao/controllers/productController.js
+++ 3_Back-end_Development/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercise/restfulzao/controllers/productController.js
@@ -3,38 +3,38 @@ const ProductModel = require('../models/productModel');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res, next) => {
+router.get('/', async (_req, res, _next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, _next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  res.json(product);
 });
 
-router.post('/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.json(newProduct);
 });
 
-router.post('/delete-user/:id', async (req, res) => {
+router.post('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  res.json(products);
 });
 
-router.post('/update-user/:id', async (req, res) => {
+router.post('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.json(products);
 });
 
 module.exports = router;
--- 3_Back-end_Development/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercise/restfulzao/index.js
+++ 3_Back-end_Development/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercise/restfulzao/index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const productController = require('./controllers/productController');
 
 const app = express();
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/products', require('./controllers/productController'));
+app.use('/products', productController);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
