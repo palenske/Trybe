87c0184830988b115e0ff2f0b9fd92b5a5e075a9
palenske <viniciuspalenske@gmail.com>
2022-02-09 16:47:05 -0300

    exercicios 1 e 2

---
+++ trybe-exercises/4_Computer_Science/bloco-32-introducao-a-python/dia-1-aprendendo-python/exercise_01.py
@@ -0,0 +1,6 @@
+def larger(a, b):
+  higher_number = max(a, b)
+  print(f'o maior número entre {a} e {b} é: {higher_number}')
+  return higher_number
+
+# larger(19221321, 4314210)
---
+++ trybe-exercises/4_Computer_Science/bloco-32-introducao-a-python/dia-1-aprendendo-python/exercise_02.py
@@ -0,0 +1,4 @@
+def list_average(numbers):
+  return sum(numbers) / len(numbers)
+
+# list_average([70, 75, 80, 90])
