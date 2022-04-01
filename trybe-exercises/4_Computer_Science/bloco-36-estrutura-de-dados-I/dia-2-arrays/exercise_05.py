3cee043462b3c8f5a9a12f3ea3a5c47fb789bf1f
palenske <viniciuspalenske@gmail.com>
2022-04-01 19:18:20 -0300

    resolve exercicio 5 com ajuda do gabarito

--- trybe-exercises/4_Computer_Science/bloco-36-estrutura-de-dados-I/dia-2-arrays/exercise_05.py
+++ trybe-exercises/4_Computer_Science/bloco-36-estrutura-de-dados-I/dia-2-arrays/exercise_05.py
@@ -1,22 +1,23 @@
 def count_servers(matrix):
-    row, column = len(matrix), len(matrix[0])
-    connected = 0
+    rows, columns = len(matrix), len(matrix[0])
+    servers_in_rows = [0 for _ in range(rows)]
+    servers_in_columns = [0 for _ in range(columns)]
 
-# resultado mais próximo que consegui:
-    for index in range(row):
-        x = []
-        row_server = matrix[index].count(1)
-        if row_server > 1:
-            connected += row_server
-        for aux in range(column):
-            x.append(matrix[aux][index])
-            # if matrix[aux][index] == matrix[index][aux]:
-            #     connected -= 1
-        col_server = x.count(1)
-        if col_server > 1:
-            connected += col_server
+    for i in range(rows):
+        for j in range(columns):
+            if matrix[i][j] == 1:
+                servers_in_rows[i] += 1
+                servers_in_columns[j] += 1
 
-    return connected
+    answer = 0
+    for i in range(rows):
+        for j in range(columns):
+            if matrix[i][j] == 1 and (
+                servers_in_rows[i] > 1 or servers_in_columns[j] > 1
+            ):
+                answer += 1
+
+    return answer
 
 
 print(count_servers([[1, 0], [1, 1]]))
